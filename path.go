f7e26f2c80536d8c833d6c8506c3a3de8a87e94b
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2021-09-10 23:41:04 -0400

    Finished Check() and its tests for the PathFragment objects. Also, created the IsInitial(), IsMaximal() and IsExecution() functions for the PathFragment objects and tested them.

--- path.go
+++ path.go
@@ -1,3 +1,4 @@
+//package modelchecking
 /*
 path.go
 Description:
@@ -5,7 +6,9 @@ Description:
 */
 package modelchecking
 
-import "fmt"
+import (
+	"fmt"
+)
 
 /*
 Type Declarations
@@ -20,13 +23,19 @@ type InfinitePathFragment struct {
 	RepeatingSuffix FinitePathFragment
 }
 
+type PathFragment interface {
+	Check() error
+	IsMaximal() bool
+	IsInitial() bool
+}
+
 // Functions
 
-func (pathIn FinitePathFragment) Check() error {
+func (fragmentIn FinitePathFragment) Check() error {
 	// Verify that the transitions in the path fragment are okay
-	for sIndex := 0; sIndex < len(pathIn.s)-1; sIndex++ {
-		si := pathIn.s[sIndex]
-		sip1 := pathIn.s[sIndex+1]
+	for sIndex := 0; sIndex < len(fragmentIn.s)-1; sIndex++ {
+		si := fragmentIn.s[sIndex]
+		sip1 := fragmentIn.s[sIndex+1]
 
 		siAncestors, err := Post(si)
 		if err != nil {
@@ -44,3 +53,122 @@ func (pathIn FinitePathFragment) Check() error {
 	// Return nothing if all transitions are correct
 	return nil
 }
+
+/*
+Check
+Description:
+	For the InfinitePathFragment object, this checks that:
+	- The sequences within UniquePrefix and RepeatingSuffix are independently valid
+	- The transition from UniquePrefix to RepeatingSuffix is valid
+	- The transition from the end of RepeatingSuffix to the beginning of RepeatingSuffix is valid
+*/
+func (fragmentIn InfinitePathFragment) Check() error {
+	// Make sure that the suffix contains at least one element
+	if len(fragmentIn.RepeatingSuffix.s) == 0 {
+		return fmt.Errorf("The RepeatingSuffix value has length 0. If this is a FinitePathFragment, then use that object instead!")
+	}
+
+	//Verify that Both the prefix and suffix are valid by themselves
+	err := fragmentIn.UniquePrefix.Check()
+	if err != nil {
+		return fmt.Errorf("There was an issue while checking the prefix of the path fragment: %v", err)
+	}
+
+	err = fragmentIn.RepeatingSuffix.Check()
+	if err != nil {
+		return fmt.Errorf("There was an issue while checking the suffix of the path fragment: %v", err)
+	}
+
+	//Check that the first state in the suffix is in the Post of the last state from the prefix
+	lastStateInPrefix := fragmentIn.UniquePrefix.s[len(fragmentIn.UniquePrefix.s)-1]
+	firstStateInSuffix := fragmentIn.RepeatingSuffix.s[0]
+	ancestorsOfLastState, err := Post(lastStateInPrefix)
+	if err != nil {
+		return fmt.Errorf("There was an error computing the Post of the last state in the prefix: %v", err)
+	}
+
+	if !firstStateInSuffix.In(ancestorsOfLastState) {
+		return fmt.Errorf("The first state in the suffix \"%v\" was not an ancestor of the last state in the prefix \"%v\".", firstStateInSuffix, lastStateInPrefix)
+	}
+
+	//Check that the first state in the suffix is in the Post of the last state in the suffix
+	lastStateInSuffix := fragmentIn.RepeatingSuffix.s[len(fragmentIn.RepeatingSuffix.s)-1]
+	ancestorsOfLastState, err = Post(lastStateInSuffix)
+	if err != nil {
+		return fmt.Errorf("There was an error computing the Post of the last state in the suffix: %v", err)
+	}
+
+	if !firstStateInSuffix.In(ancestorsOfLastState) {
+		return fmt.Errorf("The first state in the suffix \"%v\" was not an ancestor of the last state in the suffix \"%v\".", firstStateInSuffix, lastStateInSuffix)
+	}
+
+	// If you made it this far, then everything is fine.
+	return nil
+}
+
+/*
+IsMaximal
+Description:
+	For the FinitePathFragment, this is true if the last state is terminal.
+Assumption:
+	Assumes that you've already checked the FinitePathFragment.
+*/
+func (fragmentIn FinitePathFragment) IsMaximal() bool {
+	//Grabs last state in path.
+	finalState := fragmentIn.s[len(fragmentIn.s)-1]
+
+	return finalState.IsTerminal()
+}
+
+/*
+IsMaximal
+Description:
+	For the InfinitePathFragment, this is always true.
+Assumption:
+	This assumes that the InfinitePathFragment was already checked.
+*/
+func (fragmentIn InfinitePathFragment) IsMaximal() bool {
+	return true
+}
+
+/*
+IsInitial
+Description
+	For any type of execution, this is true if the first state of the execution is in the transition system's initial state set.
+*/
+func (fragmentIn FinitePathFragment) IsInitial() bool {
+	initialState := fragmentIn.s[0]
+	System := *&initialState.System
+
+	return initialState.In(System.I)
+}
+
+func (fragmentIn InfinitePathFragment) IsInitial() bool {
+	var initialState TransitionSystemState
+	if len(fragmentIn.UniquePrefix.s) > 0 {
+		initialState = fragmentIn.UniquePrefix.s[0]
+	} else {
+		initialState = fragmentIn.RepeatingSuffix.s[0]
+	}
+
+	System := *&initialState.System
+
+	return initialState.In(System.I)
+}
+
+/*
+IsPath()
+Description:
+	This function should work for either FinitePathFragment or InitialPathFragment objects and will check to
+	see if the given fragment is initial and maximal before returning true or false.
+Assumption:
+	This function assumes that fragmentIn has previously been checked.
+*/
+func IsPath(fragmentIn PathFragment) bool {
+	return fragmentIn.IsInitial() && fragmentIn.IsMaximal()
+}
+
+/*
+ToTrace
+
+*/
--- path_test.go
+++ path_test.go
@@ -5,7 +5,10 @@ Description:
 */
 package modelchecking
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 /*
 TestPath_Check1
@@ -42,5 +45,379 @@ func TestPath_Check2(t *testing.T) {
 	// Test Fragment
 	if fpf0.Check() != nil {
 		t.Errorf("The Check did not identify that there is an invalid transition in path. %v", fpf0.Check())
+	}
+}
+
+/*
+TestPath_Check3
+Description:
+	Tests whether or not an infinite path fragment with all good transitions is identified as valid.
+*/
+func TestPath_Check3(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2]},
+	}
+
+	fpf1 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[3]},
+	}
+
+	ipf0 := InfinitePathFragment{
+		UniquePrefix:    fpf0,
+		RepeatingSuffix: fpf1,
+	}
+
+	// Test Fragment
+	if ipf0.Check() != nil {
+		t.Errorf("The Check did not identify that this is a valid infinite path fragment. %v", fpf0.Check())
+	}
+}
+
+/*
+TestPath_Check4
+Description:
+	Checks an infinite path fragment where the prefix is bad.
+*/
+func TestPath_Check4(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[1]},
+	}
+
+	fpf1 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[3]},
+	}
+
+	ipf0 := InfinitePathFragment{
+		UniquePrefix:    fpf0,
+		RepeatingSuffix: fpf1,
+	}
+
+	// Test Fragment
+	err := ipf0.Check()
+	if !strings.Contains(err.Error(), "There was an issue while checking the prefix of the path fragment:") {
+		t.Errorf("The Check did not identify that this is an invalid infinite path fragment. %v", err)
+	}
+}
+
+/*
+TestPath_Check5
+Description:
+	Checks an infinite path fragment where the suffix is bad (contains bad transition in its sequence).
+*/
+func TestPath_Check5(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2]},
+	}
+
+	fpf1 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[0]},
+	}
+
+	ipf0 := InfinitePathFragment{
+		UniquePrefix:    fpf0,
+		RepeatingSuffix: fpf1,
+	}
+
+	// Test Fragment
+	err := ipf0.Check()
+	if !strings.Contains(err.Error(), "There was an issue while checking the suffix of the path fragment:") {
+		t.Errorf("The Check did not identify that this is an invalid infinite path fragment. %v", err)
+	}
+}
+
+/*
+TestPath_Check6
+Description:
+	Checks an infinite path fragment where the transition from
+	prefix to suffix is bad.
+*/
+func TestPath_Check6(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2]},
+	}
+
+	fpf1 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[1], ts0.S[3], ts0.S[0]},
+	}
+
+	ipf0 := InfinitePathFragment{
+		UniquePrefix:    fpf0,
+		RepeatingSuffix: fpf1,
+	}
+
+	// Test Fragment
+	err := ipf0.Check()
+	if !strings.Contains(err.Error(), "The first state in the suffix \"select\" was not an ancestor of the last state in the prefix \"beer\".") {
+		t.Errorf("The Check did not identify that this is an invalid infinite path fragment. %v", err)
+	}
+}
+
+/*
+TestPath_Check7
+Description:
+	Checks an infinite path fragment where the transition from
+	prefix to suffix is bad.
+*/
+func TestPath_Check7(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2]},
+	}
+
+	fpf1 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[3], ts0.S[0]},
+	}
+
+	ipf0 := InfinitePathFragment{
+		UniquePrefix:    fpf0,
+		RepeatingSuffix: fpf1,
+	}
+
+	// Test Fragment
+	err := ipf0.Check()
+	if !strings.Contains(err.Error(), "The first state in the suffix \"pay\" was not an ancestor of the last state in the suffix \"pay\".") {
+		t.Errorf("The Check did not identify that this is an invalid infinite path fragment. %v", err)
+	}
+}
+
+/*
+TestPath_IsMaximal1
+Description:
+	Checks to see if a finitepathfragment which is known to be not maximal is maximal.
+*/
+func TestPath_IsMaximal1(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2]},
+	}
+
+	// Test
+	if fpf0.IsMaximal() {
+		t.Errorf("The FinitePathFragment is expected to NOT BE maximal, but the function claims it is.")
+	}
+}
+
+/*
+TestPath_IsMaximal2
+Description:
+	Checks to see if a finitepathfragment which is known to be maximal is maximal.
+*/
+func TestPath_IsMaximal2(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := TransitionSystem_GetSimpleTS3()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2], ts0.S[3]},
+	}
+
+	// Test
+	if fpf0.Check() != nil {
+		t.Errorf("fpf0 failed the check! %v", fpf0.Check())
+	}
+
+	if !fpf0.IsMaximal() {
+		t.Errorf("The FinitePathFragment is expected to BE maximal, but the function claims it is not.")
+	}
+}
+
+/*
+TestPath_IsMaximal3
+Description:
+	Checks to see if an infinitepathfragment is maximal.
+*/
+func TestPath_IsMaximal3(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2]},
+	}
+
+	fpf1 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[3]},
+	}
+
+	ipf0 := InfinitePathFragment{
+		UniquePrefix:    fpf0,
+		RepeatingSuffix: fpf1,
+	}
+
+	// Test Fragment
+	if ipf0.Check() != nil {
+		t.Errorf("ipf0 failed the check! %v", ipf0.Check())
+	}
+
+	if !ipf0.IsMaximal() {
+		t.Errorf("ipf0 is maximal, but IsMaximal() claims it is not!")
+	}
+
+}
+
+/*
+TestPath_IsInitial1
+Description:
+	Checks to see if a finitepathfragment which is known to be initial is initial.
+*/
+func TestPath_IsInitial1(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2]},
+	}
+
+	// Test
+	if !fpf0.IsInitial() {
+		t.Errorf("The FinitePathFragment is expected to BE initial, but the function claims it is not.")
+	}
+}
+
+/*
+TestPath_IsInitial2
+Description:
+	Checks to see if a finitepathfragment which is known to be not initial is initial.
+*/
+func TestPath_IsInitial2(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[1], ts0.S[2], ts0.S[0]},
+	}
+
+	// Test
+	if fpf0.IsInitial() {
+		t.Errorf("The FinitePathFragment is expected to NOT BE initial, but the function claims it is.")
+	}
+}
+
+/*
+TestPath_IsInitial3
+Description:
+	Checks to see if an infinitepathfragment is initial.
+*/
+func TestPath_IsInitial3(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2]},
+	}
+
+	fpf1 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[3]},
+	}
+
+	ipf0 := InfinitePathFragment{
+		UniquePrefix:    fpf0,
+		RepeatingSuffix: fpf1,
+	}
+
+	// Test Fragment
+	if ipf0.Check() != nil {
+		t.Errorf("ipf0 failed the check! %v", ipf0.Check())
+	}
+
+	if !ipf0.IsInitial() {
+		t.Errorf("ipf0 is initial, but IsInitial() claims it is not!")
+	}
+
+}
+
+/*
+TestPath_IsInitial4
+Description:
+	Checks to see if an infinitepathfragment is initial.
+*/
+func TestPath_IsInitial4(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[1], ts0.S[2]},
+	}
+
+	fpf1 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[3]},
+	}
+
+	ipf0 := InfinitePathFragment{
+		UniquePrefix:    fpf0,
+		RepeatingSuffix: fpf1,
+	}
+
+	// Test Fragment
+	if ipf0.Check() != nil {
+		t.Errorf("ipf0 failed the check! %v", ipf0.Check())
+	}
+
+	if ipf0.IsInitial() {
+		t.Errorf("ipf0 is initial, but IsInitial() claims it is not!")
+	}
+
+}
+
+/*
+TestPath_IsPath1
+Description:
+	Checks to see if a finitepathfragment which is known to be initial but not maximal, is a path.
+	Answer should be no.
+*/
+func TestPath_IsPath1(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2]},
+	}
+
+	// Test
+	if IsPath(fpf0) {
+		t.Errorf("The FinitePathFragment is expected to BE initial, but the function claims it is not.")
+	}
+}
+
+/*
+TestPath_IsPath2
+Description:
+	Checks to see if a finitepathfragment which is known to be initial but not maximal, is a path.
+	Answer should be no.
+*/
+func TestPath_IsPath2(t *testing.T) {
+	// Create an example FinitePathFragment object
+	ts0 := GetBeverageVendingMachineTS()
+
+	fpf0 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[2]},
+	}
+
+	fpf1 := FinitePathFragment{
+		s: []TransitionSystemState{ts0.S[0], ts0.S[1], ts0.S[3]},
+	}
+
+	ipf0 := InfinitePathFragment{
+		UniquePrefix:    fpf0,
+		RepeatingSuffix: fpf1,
+	}
+
+	// Test
+	if !IsPath(ipf0) {
+		t.Errorf("The InfinitePathFragment is expected to be a path, as it is both initial and maximal, but the function claims it is not.")
 	}
 }
